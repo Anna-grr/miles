24ce261374a087058c64d9cbde872ee26dfe159b
Anna Smirnova <anna@grraoo.ru>
2020-12-11 20:05:48 +0300

    Added src/Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 2060;
+        int bonus = 20;
+        int miles = ticket / bonus;
+        System.out.println(miles);
+    }
+}
+
